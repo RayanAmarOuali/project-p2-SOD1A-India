e0f83e379e3f7272bea63ecef404eee519a8b649
RayanAmarOuali <9021944@student.zadkine.nl>
2026-02-03 13:02:24 +0100

    changes

--- script.js
+++ script.js
@@ -3,4 +3,22 @@ fetch('footer.html')
   .then(data => {
     document.querySelector('.footer-container').innerHTML = data;
   })
+
+  fetch('menu.html')
+  .then(response => response.text())
+  .then(data => {
+    document.querySelector('.menu-container').innerHTML = data;
+  })
+
+
+let knop = document.getElementById("verzenden");
+
+knop.addEventListener("click", klik); 
+ 
+
+function klik() {
+    alert("Bedankt voor uw donatie!");
+    return
+    
+}
   
